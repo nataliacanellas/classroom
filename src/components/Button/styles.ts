a8d2cafb8c138aac6b40ea9ad47ccc0e606d9fd2
Natalia F. Canellas <nataliafcanellas@gmail.com>
2023-07-18 11:44:05 -0300

    adjusts style

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,5 +1,5 @@
+import styled, { css } from 'styled-components/native'
 import { TouchableOpacity } from 'react-native'
-import styled from 'styled-components/native'
 
 export type ButtonTypeStyleProps = 'primary' | 'secondary'
 
@@ -23,7 +23,9 @@ export const Container = styled(TouchableOpacity)<Props>`
 `
 
 export const Title = styled.Text`
-  font-size: ${({ theme }) => theme.font_size.md}px;
-  color: ${({ theme }) => theme.colors.white};
-  font-family: ${({ theme }) => theme.font_family.bold};
+  ${({ theme }) => css`
+    font-size: ${theme.font_size.md}px;
+    color: ${theme.colors.white};
+    font-family: ${theme.font_family.bold};
+  `}
 `
